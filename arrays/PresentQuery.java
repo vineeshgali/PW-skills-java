b340e15896336b83649824b5fa6991b69a9cacc7
vineeshgali <vineeshhg@gmail.com>
2024-09-12 08:42:51 +0530

    Add files via upload

--- arrays/PresentQuery.java
+++ arrays/PresentQuery.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class PresentQuery {
+    static int[] makeFrequencyArray(int[] arr){
+        int[] freq = new int[100005];
+        for(int i = 0; i < arr.length; i++){
+            freq[arr[i]]++;
+        }
+        return freq;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,6 +19,22 @@ public class PresentQuery {
         System.out.println("Enter " + n + " elements");
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
+        }
+
+        int[] freq = makeFrequencyArray(arr);
+
+        System.out.print("Enter number of queries ");
+        int q = sc.nextInt();
+
+        while(q > 0){
+            System.out.print("Enter number to be searched ");
+            int x = sc.nextInt();
+            if(freq[x] > 0){
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            q--;
         }
 
     }
